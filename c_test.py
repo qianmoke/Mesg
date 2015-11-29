9db0ac771d31406b4a92123dfe359fd64903d8af
qianmoke <zzilovey@gmail.com>
2015-11-29 22:47:44 +0800

    version1.0 realese

--- c_test.py
+++ c_test.py
@@ -1,11 +1,13 @@
-#coding=utf-8
 import socket
 from struct import *
 import datetime
 import pcapy
 import sys
 import re
+import xlwt,xlrd
+from xlutils.copy import copy
 from bs4 import BeautifulSoup
+from bs4 import UnicodeDammit
 
 def main(argv):
   #list all devices
@@ -15,7 +17,7 @@ def main(argv):
     #print "<%s>Dev:%s,Network:%s" %(devs.index(i),i,pcapy.open_live(i,0,0,0).getnet())
   idx = 0 #raw_input("Your choice : ")
   dev = devs[int(idx)]
-	
+  xls_name="Mesg.xls"	
   '''
   open device
   # Arguments here are:
@@ -28,11 +30,14 @@ def main(argv):
   cap.setfilter("tcp port 80")
   print "Listening on %s" %dev
   http_pattern=re.compile('HTTP/1.1')
-  page_pattern=re.compile(r'<html xmlns:msxsl="urn:schemas-microsoft-com:xslt" xmlns:msgs="ovow_webconsole" xmlns:userdate="ovow_webconsole_date">.*?</html>',re.S)
+  #page_pattern=re.compile(r'<html xmlns:msxsl="urn:schemas-microsoft-com:xslt" xmlns:msgs="ovow_webconsole" xmlns:userdate="ovow_webconsole_date">.*?</html>',re.S)
+  page_pattern=re.compile(r'<html xmlns:msxsl="urn:schemas-microsoft-com:xslt" xmlns:user="ovow_webconsole" xmlns:userdate="ovow_webconsole_date">.*?</html>',re.S)
   #start sniffing packets
   html_count=0
   html_dic={}
-  html_list=[]
+  mesg_dic={}
+  mesg=[]
+  mesg_writed=init(xls_name)
   while(1) :
     (header, packet) = cap.next()
     #print ('%s: captured %d bytes, truncated to %d bytes' %(datetime.datetime.now(), header.getlen(), header.getcaplen()))
@@ -43,26 +48,85 @@ def main(argv):
           html=re.findall(page_pattern,html_dic[html_count])
           html_dic={}
           if html:
-            #html_filename="msg_page"+str(html_count)+".html"
-            #html_file=file(html_filename,'w+')
-            #html_file.write(html[0])
-            #html_file.close()
-            #print html_filename
-            #print html[0]
-            print html[0]
-            soup=BeautifulSoup(html[0],'lxml')
-            print soup.prettify().decode('utf-8','ignore')
-            table_html=soup.table
-            table_soup=BeautifulSoup(str(table_html),'lxml')
-            table_txt=table_soup.get_txt()
-            print table_txt.split()
-            #print html_dic[html_count]            
+            mesg_all=collect_mesg_from_html(html)
+            if mesg_all:
+              mesg=choose_data_in_mesglist(mesg_all)
+              time=mesg[2]
+              if time not in mesg_dic.keys():
+                mesg_dic[time]=[]
+                for mesg_data in mesg:
+                  mesg_dic[time].append(mesg_data)
+                #print mesg_dic
+                #print
+                write_mesg_to_xls(mesg_writed,mesg_dic,xls_name)
+                mesg_writed.append(time)
         html_count +=1
       if html_dic.has_key(html_count):
         html_dic[html_count] +=http_packet
       else:
         html_dic[html_count]=http_packet
-      
+
+#function to init mesg_writed:
+def init(xls):
+  wlist=[]
+  today=str(datetime.date.today())
+  xls_old=xlrd.open_workbook(xls)
+  sheet=xls_old.sheet_by_name(today)
+  for time in sheet.col(2):
+    wlist.append(time.value)
+  return wlist
+  
+#function write mesg to xls
+def write_mesg_to_xls(wlist,dic,xls):
+  today=str(datetime.date.today())
+  xls_old=xlrd.open_workbook(xls)
+  sheet_num=xls_old.nsheets
+  xls_new=copy(xls_old)
+  if today not in xls_old.sheet_names():
+    sheet=xls_new.add_sheet(today)
+    xls_new.save(xls)
+    row=0
+  else:
+    sheet=xls_new.get_sheet(sheet_num-1)
+    row=xls_old.sheet_by_name(today).nrows
+
+  col=0
+  for time in dic.keys():
+    if time not in wlist:
+      print wlist
+      for mesg_data in dic[time]:
+        sheet.write(row,col,mesg_data)
+        col +=1
+      col=0
+      row +=1
+      print "write messge:%s successful"%time
+  xls_new.save(xls)
+  
+        
+#fubction to collect mesg from html 
+def collect_mesg_from_html(html):
+  table_line=[]
+  dammit = UnicodeDammit(html[0])
+  html_data=dammit.unicode_markup
+  #print dammit.original_encoding
+  soup=BeautifulSoup(html_data,'lxml')
+  #print soup.prettify()
+  for table_html in soup.find_all('table'):
+    if table_html.has_attr('cellpadding'):
+      if table_html['cellpadding']=='0':
+        #print table_html.attrs
+        table_soup=BeautifulSoup(str(table_html),'lxml')
+        table_text=table_soup.get_text() 
+        return table_text.splitlines()
+
+def choose_data_in_mesglist(mesg_list):
+  title_list=[14,16,38,58]
+  item_list=[15,17,39,62]
+  mesg=[]
+  for item in item_list:
+    mesg.append(mesg_list[item])
+  return mesg
+    
 
 #function to parse a packet
 def parse_packet(packet) :	
